e497c0c8a16460b4571c2ba941b0d4bb78668034
n0spaces <7495188+n0spaces@users.noreply.github.com>
2023-06-06 19:12:22 -0500

    Add demo code that tracks the debug adapter

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -2,9 +2,36 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+class PedagogicalDebugAdapterTrackerFactory implements vscode.DebugAdapterTrackerFactory {
+	createDebugAdapterTracker(session: vscode.DebugSession): vscode.ProviderResult<vscode.DebugAdapterTracker> {
+		return {
+			onDidSendMessage: message => {
+				vscode.window.showInformationMessage(`Reported message type: ${message.type} (${typeof message})`);
+			},
+			onWillReceiveMessage: message => console.log("Will receive message: " + message),
+			onWillStartSession: () => vscode.window.showInformationMessage("Will start session"),
+			onWillStopSession: () => vscode.window.showInformationMessage("Will stop session"),
+			onError: error => vscode.window.showErrorMessage(`Error: ${error.name} ${error.message}`),
+			onExit: (code, signal) => vscode.window.showInformationMessage(`Debug adapter exited with code ${code}: ${signal}`),
+		};
+	}
+};
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	vscode.debug.onDidStartDebugSession(debugSession => {
+		console.log("Starting debugging type: " + debugSession.type);
+	});
+
+	vscode.debug.onDidReceiveDebugSessionCustomEvent(ev => console.log("custom event " + ev.event + " " + ev.body));
+	vscode.debug.onDidTerminateDebugSession(debugSession => console.log("Terminated debug session: " + debugSession.name));
+	vscode.debug.onDidChangeActiveDebugSession(debugSession => console.log(debugSession));
+
+	let debugAdapterTracker = new PedagogicalDebugAdapterTrackerFactory();
+	//context.subscriptions.push(vscode.debug.registerDebugAdapterTrackerFactory('python', debugAdapterTracker));
+	//context.subscriptions.push(vscode.debug.registerDebugAdapterTrackerFactory('mock', debugAdapterTracker));
+	context.subscriptions.push(vscode.debug.registerDebugAdapterTrackerFactory('*', debugAdapterTracker));
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -17,11 +44,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-debug-attacher-extension-test in a web extension host!');
+		console.log('Hello World from vscode-debug-attacher-extension-test in a web extension host!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
